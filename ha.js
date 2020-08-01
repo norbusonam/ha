b5d81e7e6aaa8fee7b1727fceeb821df77477ad5
Norbu Sonam <nsonam1822@gmail.com>
2020-08-01 19:42:35 -0400

    get jokes from API and print

---
+++ ha.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+const axios = require('axios')
+
+const jokeUrl = 'https://official-joke-api.appspot.com/random_joke'
+
+function tellJoke(joke) {
+  console.log(joke.setup)
+  console.log(joke.punchline);
+}
+
+// Get joke
+axios.get(jokeUrl)
+.then(res => {
+  tellJoke(res.data)
+})
+.catch(err => {
+  console.error('😢 Unable to get joke')
+})
